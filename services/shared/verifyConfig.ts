0d45e7b74981b8be98300241f31af0a96822c148
Philipp Hergenhahn <philipp.hergenhahn@setilic.com>
2024-05-10 11:29:00 +0200

    Refactored environment variable verification process
    The 'verifyConfig' function was moved from the 'vattenfall' service to the 'shared' module, allowing it to be reused by other services. The process of verifying environment variables was also updated to be more dynamic in 'index.ts' of the 'vattenfall' service.

---
+++ services/shared/verifyConfig.ts
@@ -0,0 +1,24 @@
+function red(text: string) {
+  return "\x1b[31m" + text + "\x1b[0m";
+}
+
+function green(text: string) {
+  return "\x1b[32m" + text + "\x1b[0m";
+}
+
+export type ServiceEnv = { name: string; value?: string };
+
+export function verifyConfig(envs: ServiceEnv[]): string[] {
+  for (let i = 0; i < envs.length; i++) {
+    const v = envs[i];
+    if (!v.value) {
+      console.error(red("Environment Error:"), v.name, red("is missing"));
+      // noinspection TypeScriptUnresolvedReference
+      return process.exit(1);
+    } else {
+      console.log(green("Environment:"), v.name, green("is set to"), v.value);
+    }
+  }
+
+  return envs.map((env) => env.value);
+}
--- services/vattenfall/src/index.ts
+++ services/vattenfall/src/index.ts
@@ -1,26 +1,32 @@
 import dotenv from 'dotenv';
 import axios from 'axios';
-import { verifyConfig } from './verifyConfig';
-import cron from "node-cron"
-import { ServiceDataRequestBody, ServiceIncident } from '@globalTypes/ServiceDataRequest';
+import { ServiceEnv, verifyConfig } from '../../shared/verifyConfig';
+import cron from 'node-cron';
+import {
+  ServiceDataRequestBody,
+  ServiceIncident,
+} from '@globalTypes/ServiceDataRequest';
 
 dotenv.config();
 
-const { VATTENFALL_API_URL, FETCH_INTERVALL, SERVICE_TOKEN, ENDPOINT_URL } = process.env;
+const vars: ServiceEnv[] = [
+  { name: 'VATTENFALL_API_URL', value: process.env.VATTENFALL_API_URL },
+  { name: 'FETCH_INTERVALL', value: process.env.FETCH_INTERVALL },
+  { name: 'SERVICE_TOKEN', value: process.env.SERVICE_TOKEN },
+  { name: 'ENDPOINT_URL', value: process.env.ENDPOINT_URL },
+];
+
+const [VATTENFALL_API_URL, FETCH_INTERVALL, SERVICE_TOKEN, ENDPOINT_URL] =
+  verifyConfig(vars);
 
 const skippedDescriptions = ['Kommande planerat avbrott'];
 
-if (!verifyConfig()) {
-  process.exit(1);
-}
-
-console.log("Service started at", new Date().toISOString());
+console.log('Service started at', new Date().toISOString());
 
 async function fetchData() {
   console.log('Requesting raw data at', new Date().toISOString());
   try {
-    const res = await axios.get(VATTENFALL_API_URL);
-    return res.data;
+    return await axios.get(VATTENFALL_API_URL).then(res => res.data);
   } catch (err) {
     console.error('Error while fetching data:', err);
     throw new Error(err);
@@ -28,45 +34,67 @@ async function fetchData() {
 }
 
 async function sendData(data: ServiceIncident[]) {
-  await axios.post(ENDPOINT_URL, {
-    token: SERVICE_TOKEN,
-    incidents: data,
-  } satisfies ServiceDataRequestBody)
+  await axios
+    .post(ENDPOINT_URL, {
+      token: SERVICE_TOKEN,
+      incidents: data,
+    } satisfies ServiceDataRequestBody)
     .then(() => {
-      console.log('Successfully reported', data.length, 'incidents at', new Date().toISOString());
+      console.log(
+        'Successfully reported',
+        data.length,
+        'incidents at',
+        new Date().toISOString(),
+      );
     })
-    .catch((err) => {
-      console.error('Error while reporting data at', new Date().toISOString() + ':', err.message);
+    .catch(err => {
+      console.error(
+        'Error while reporting data at',
+        `${new Date().toISOString()}:`,
+        err.message,
+      );
     });
 }
 
-
 cron.schedule(FETCH_INTERVALL, () => {
   fetchData().then(async res => {
-    const relevantData = res.warnings.warning.filter(item => !skippedDescriptions.includes(item.description));
+    const relevantData = res.warnings.warning.filter(
+      item => !skippedDescriptions.includes(item.description),
+    );
 
     if (!relevantData.length) {
       console.log('No data to report at', new Date().toISOString());
       return;
     }
 
-    console.log("Transforming", relevantData.length, "data entries for reporting");
+    console.log(
+      'Transforming',
+      relevantData.length,
+      'data entries for reporting',
+    );
 
-    const transformedData: ServiceIncident[] = relevantData.map(item => ({
-      location: !!item.placenames.length ? item.placenames?.split(', ') : undefined,
-      affected_customers: item.affectedCustomers,
-      description: item.description,
-      additional_information: item.freeText,
-      start_time: item.startTime,
-      end_time: !!item.completionTime.length ? item.completionTime : undefined,
-      area_codes: item.affectedAreas.map((area: { areacode: number; }) => area.areacode),
-      planned: false,
-    } satisfies ServiceIncident));
+    const transformedData: ServiceIncident[] = relevantData.map(
+      item =>
+        ({
+          location: !!item.placenames.length
+            ? item.placenames?.split(', ')
+            : undefined,
+          affected_customers: item.affectedCustomers,
+          description: item.description,
+          additional_information: item.freeText,
+          start_time: item.startTime,
+          end_time: !!item.completionTime.length
+            ? item.completionTime
+            : undefined,
+          area_codes: item.affectedAreas.map(
+            (area: { areacode: number }) => area.areacode,
+          ),
+          planned: false,
+        }) satisfies ServiceIncident,
+    );
 
     if (!!transformedData.length) {
       await sendData(transformedData);
     }
-
   });
 });
-
