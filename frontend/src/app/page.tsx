827ae81b932cd4eed00e6cf8d63cf3d672645fc5
Johannes Thorén <Johannes@lgjt.xyz>
2024-05-11 20:42:25 +0200

    Made a basic frontend, Needs improvements a simple list-view to display the incidents and a small stats component that curretly only displays number of outages

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,11 +1,18 @@
 import client from '@prisma/prismadb';
+import IncidentList from './_components/incidentList';
+import Stats from './_components/stats';
 
 export default async function Home() {
   const value = await client.incidents.findMany();
 
-  return (
-    <div>
-      <pre>{JSON.stringify(value, null, 2)}</pre>
+  return (<>
+    <div className='w-3/4 shadow-md mx-auto max-h-dvh rounded-md'>
+      <Stats />
     </div>
+    <div className='w-3/4 mx-auto max-h-dvh'>
+      <IncidentList />
+    </div>
+  </>
+
   );
 }
