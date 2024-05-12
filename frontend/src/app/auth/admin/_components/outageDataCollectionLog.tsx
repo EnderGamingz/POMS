b0e3a75942b04e29f0d16badcbd020c9052a1a45
Johannes Thorén <Johannes@lgjt.xyz>
2024-05-12 20:26:10 +0200

    small fixes

--- frontend/src/app/auth/admin/_components/outageDataCollectionLog.tsx
+++ frontend/src/app/auth/admin/_components/outageDataCollectionLog.tsx
@@ -10,12 +10,34 @@ export default async function () {
   });
 
   return (
-    <div className='col-span-3 row-span-1'>
+    <div className='col-span-3 row-span-2'>
       <header className='flex h-10 w-full  items-center justify-center rounded-md rounded-b-none bg-gray-700 text-gray-200'>
         <p>Outage Collection Log</p>
       </header>
-      <div className='overflow-y-auto max-h-svh  h-full w-full px-2 py-2'>
-        <table className='w-full'>
+      <div className='text-center h-full w-full px-2 py-2'>
+        <div>
+          <div className='outageDataCollectionLogRow grid w-full grid-cols-5 shadow-md rounded-md text-sm'>
+            <span >Id</span>
+            <span>Time</span>
+            <span>Info</span>
+            <span>Service ID</span>
+            <span>Service</span>
+          </div>
+
+          <div className='max-h-64 overflow-auto text-xs'>
+            {log.map(l => (
+              <div className='outageDataCollectionLogRow grid w-full grid-cols-5 shadow-sm  my-2 py-1 rounded-md  even:bg-gray-200 odd:bg-gray-100'>
+                <span>{l.id}</span>
+                <span>{l.time.toISOString()}</span>
+                <span>{l.info}</span>
+                <span>{l.serviceId}</span>
+                <span>{l.service.name}</span>
+              </div>
+            )).reverse()}
+          </div>
+        </div>
+
+        {/* <table className='w-full'>
           <thead>
             <tr>
               <th>Id</th>
@@ -25,8 +47,8 @@ export default async function () {
               <th>Service name</th>
             </tr>
           </thead>
-    
-          <tbody className='text-center text-xs'>
+
+          <tbody className='text-center text-xs '>
             {log
               .map(l => (
                 <tr className='border-b-2'>
@@ -39,7 +61,7 @@ export default async function () {
               ))
               .reverse()}
           </tbody>
-        </table>
+        </table> */}
       </div>
     </div>
   );
